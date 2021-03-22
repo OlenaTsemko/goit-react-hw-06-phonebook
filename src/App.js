30f1e34e77b18a8318b336a9968616d5d7f5e4d4
Lena <65902225+OlenaTsemko@users.noreply.github.com>
2021-03-22 16:32:38 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'; // - необязательно по-новому
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App - главный компонент на странице</h1>
+    </div>
+  );
+}
+
+export default App;
